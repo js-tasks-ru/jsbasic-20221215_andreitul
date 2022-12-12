f3ffa2cf6c187a67029165e698395d8c85332f26
Andrew Tulintsev <andreitul@inbox.ru>
2022-12-12 20:52:03 +0300

    Выполнена задача N2

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -9,8 +9,12 @@ function print(text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
-function isValid(name) {
-  // ваш код...
+function isValid(name) { 
+if (name == ("" || null)) { 
+  return false 
+} else if (((name.length < 4) || (name == ("" || null)) || (name.includes(" "))) == true) {
+  return false 
+} else { return true }
 }
 
 function sayHello() {
